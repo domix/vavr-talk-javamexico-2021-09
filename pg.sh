8162500fa978e7f3e31766c3e8d13902b93fa36e
Domingo Suarez Torres <domingo.suarez@gmail.com>
2021-09-26 23:13:01 -0500

    Code enhancements

--- pg.sh
+++ pg.sh
@@ -1,8 +1,10 @@
 
 docker run --rm \
+  --name vavr-postgres \
   -e POSTGRES_PASSWORD=secret \
   -e POSTGRES_USER=user \
   -e POSTGRES_DB=db \
   -p 5423:5432 \
+  -v $(pwd)/datapg:/var/lib/postgresql/data \
   -d \
   postgres:13.4-alpine3.14
