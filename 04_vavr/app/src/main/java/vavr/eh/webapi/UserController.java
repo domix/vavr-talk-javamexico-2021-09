e54330cbe30c08bc97f3f9dbdb174bcb5b2518a5
Domingo Suarez Torres <domingo.suarez@gmail.com>
2021-09-26 21:46:32 -0500

    Code enhancements

--- 04_vavr/app/src/main/java/vavr/eh/webapi/UserController.java
+++ 04_vavr/app/src/main/java/vavr/eh/webapi/UserController.java
@@ -4,8 +4,13 @@ import io.micronaut.core.annotation.NonNull;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.*;
 import lombok.RequiredArgsConstructor;
+import vavr.eh.Failure;
+import vavr.eh.domain.User;
 import vavr.eh.service.UserManagementService;
 import vavr.eh.webapi.domain.AddUserCommand;
+import vavr.eh.webapi.domain.UserDTO;
+
+import java.util.Map;
 
 @Controller(UserController.URI)
 @RequiredArgsConstructor
@@ -19,11 +24,9 @@ public class UserController {
   public HttpResponse<?> addUser(final @NonNull @Body AddUserCommand command) {
     return userManagementService.addUser(UserWebApiMapper.of(command))
         .fold(
-            failure -> HttpResponse.badRequest(),
-            saved -> {
-              final var location = HttpResponse.uri("%s/%s".formatted(URI, saved.idAsString()));
-              return HttpResponse.created(UserWebApiMapper.of(saved), location);
-            });
+            this::userAlreadyExists,
+            this::savedUser
+        );
   }
 
   @NonNull
@@ -32,5 +35,17 @@ public class UserController {
     return userManagementService.findById(id)
         .map(user -> HttpResponse.ok(UserWebApiMapper.of(user)))
         .orElseGet(HttpResponse::notFound);
+  }
+
+  private HttpResponse<UserDTO> savedUser(User saved) {
+    final var location = HttpResponse.uri("%s/%s".formatted(URI, saved.idAsString()));
+    return HttpResponse.created(UserWebApiMapper.of(saved), location);
+  }
+
+  private HttpResponse<Map<String, String>> userAlreadyExists(Failure failure) {
+    final var body = Map.of(
+        "message", failure.getReason()
+    );
+    return HttpResponse.badRequest(body);
   }
 }
